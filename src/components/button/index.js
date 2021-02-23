27364e8bf102a6568588ae6d68bfbdf386bf8da4
Vsk Code <vynycyusvieira@gmail.com>
2021-02-23 20:08:44 -0300

    🧮 Create calculator project

---
+++ src/components/button/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style/styled.css'
+
+export default props =>
+    <button onClick={e => props.click && props.click(props.label)}
+    className={`button 
+    ${props.operation ? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}
+`}>{props.label}</button>
---
+++ src/components/display/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import './style/styled.css'
+
+export default props =>
+    <div className='display'>{props.value}</div>
